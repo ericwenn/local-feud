65c88e88097449a2e2fa212f31de1dfaa89d5dc8
Alfred Björk <alfbjo@student.chalmers.se>
2016-04-12 14:01:21 +0200

    User classes

--- project/app/src/main/java/com/chalmers/tda367/localfeud/data/User.java
+++ project/app/src/main/java/com/chalmers/tda367/localfeud/data/User.java
@@ -4,4 +4,44 @@ package com.chalmers.tda367.localfeud.data;
  * Created by Alfred on 2016-04-11.
  */
 public class User {
+    int id;
+    int age;
+    Sex sex;
+
+    public enum Sex{
+        MALE, FEMALE
+    }
+
+    public User (int id, int age, Sex sex){
+        this.id = id;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
+    public int getAge(){
+        return this.age;
+    }
+
+    public Sex getSex(){
+        return this.sex;
+    }
+
+    public boolean equals(Object o){
+        if (o == null){
+            return false;
+        }
+        else if (o == this){
+            return true;
+        }
+        else if (o instanceof User){
+            return ((User) o).getId() == this.getId();
+        }
+        else{
+            return false;
+        }
+    }
 }
