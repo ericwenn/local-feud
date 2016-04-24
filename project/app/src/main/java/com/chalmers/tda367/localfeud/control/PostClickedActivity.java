ab544fc457dcfe11a7f3397e5ebd7d0944c191a4
David Söderberg <dasovm@gmail.com>
2016-04-24 13:24:34 +0200

    Toolbar add to PostClickedActivity

--- project/app/src/main/java/com/chalmers/tda367/localfeud/control/PostClickedActivity.java
+++ project/app/src/main/java/com/chalmers/tda367/localfeud/control/PostClickedActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.widget.TextView;
 
@@ -18,22 +19,28 @@ import java.util.concurrent.TimeUnit;
 /**
  * Created by Daniel Ahlqvist on 2016-04-18.
  */
-public class PostClickedActivity extends AppCompatActivity
-{
+public class PostClickedActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private PostClickedAdapter postClickedAdapter;
     private Post post;
-    private TextView postText, senderText, distanceText, timeText, timeElapsedText;
+    private TextView postText, senderText, distanceText, timeText, timeElapsedText, toolbarTextView;
+    private Toolbar toolbar;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent intent = this.getIntent();
         Bundle bundle = intent.getExtras();
         post = (Post) bundle.getSerializable("post");
         //Log.d(TagHandler.MAIN_TAG, "Postid: " + post.getId());
         setContentView(R.layout.activity_post_clicked);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbarTextView = (TextView) findViewById(R.id.toolbar_title_textview);
+        toolbarTextView.setText("Post ID: " + post.getId());
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(R.string.empty_string);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
@@ -56,23 +63,18 @@ public class PostClickedActivity extends AppCompatActivity
 
         String timeSinceUpload;
 
-        if(timeElapsedDay >= 1)
-        {
-            if(timeElapsedDay == 1)
+        if (timeElapsedDay >= 1) {
+            if (timeElapsedDay == 1)
                 timeSinceUpload = "1 day ago";
             else
                 timeSinceUpload = timeElapsedDay + " days ago";
-        }
-        else if(timeElapsedMin > 60)
-        {
-            if(timeElapsedHour == 1)
+        } else if (timeElapsedMin > 60) {
+            if (timeElapsedHour == 1)
                 timeSinceUpload = "1 hour ago";
             else
                 timeSinceUpload = timeElapsedHour + " hours ago";
-        }
-        else
-        {
-            if(timeElapsedMin == 1)
+        } else {
+            if (timeElapsedMin == 1)
                 timeSinceUpload = "1 minute ago";
             else
                 timeSinceUpload = timeElapsedMin + " minutes ago";
@@ -93,8 +95,7 @@ public class PostClickedActivity extends AppCompatActivity
         recyclerView = (RecyclerView) findViewById(R.id.comment_feed_recyclerview);
         if (recyclerView != null) {
             recyclerView.setHasFixedSize(true);
-        }
-        else {
+        } else {
             Log.e(TagHandler.MAIN_TAG, "No RecyclerView found in activity_post_clicked.xml");
         }
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
