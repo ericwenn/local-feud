f75503392e124be4a1b6d06e7d695e942ab68ce8
Eric Wennerberg <ericwenn@gmail.com>
2016-04-27 12:14:23 +0200

    Add PermissionFlow activity

---
+++ project/app/src/main/java/com/chalmers/tda367/localfeud/permissionflow/PermissionFlow.java
@@ -0,0 +1,59 @@
+package com.chalmers.tda367.localfeud.permissionflow;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import com.github.paolorotolo.appintro.AppIntro;
+import com.github.paolorotolo.appintro.AppIntroFragment;
+
+public class PermissionFlow extends AppIntro {
+
+    // Please DO NOT override onCreate. Use init.
+    @Override
+    public void init(Bundle savedInstanceState) {
+
+
+        // Test slide
+        String title = "Permission Flow";
+        String description = "Testar permission flow";
+
+        addSlide(AppIntroFragment.newInstance(title, description, 0, 0));
+
+        // OPTIONAL METHODS
+        // Override bar/separator color.
+        setBarColor(Color.parseColor("#3F51B5"));
+        setSeparatorColor(Color.parseColor("#2196F3"));
+
+        // Hide Skip/Done button.
+        showSkipButton(false);
+        setProgressButtonEnabled(false);
+
+        // Turn vibration on and set intensity.
+        // NOTE: you will probably need to ask VIBRATE permisssion in Manifest.
+        setVibrate(true);
+        setVibrateIntensity(30);
+    }
+
+
+
+    @Override
+    public void onSkipPressed() {
+        // Do something when users tap on Skip button.
+    }
+
+    @Override
+    public void onDonePressed() {
+        // Do something when users tap on Done button.
+    }
+
+    @Override
+    public void onSlideChanged() {
+        // Do something when the slide changes.
+    }
+
+    @Override
+    public void onNextPressed() {
+        // Do something when users tap on Next button.
+    }
+
+}
