4d88e1add338aab00b37dcee4199d20e8f52a410
Daniel Ahlqvist <danahlq@student.chalmers.se>
2016-05-08 14:14:45 +0200

    Underlättar för testning och postar meddelande till snackbar

--- project/app/src/main/java/com/chalmers/tda367/localfeud/control/ChatActiveActivity.java
+++ project/app/src/main/java/com/chalmers/tda367/localfeud/control/ChatActiveActivity.java
@@ -1,9 +1,17 @@
 package com.chalmers.tda367.localfeud.control;
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
 
 import com.chalmers.tda367.localfeud.R;
+import com.chalmers.tda367.localfeud.data.Comment;
+import com.chalmers.tda367.localfeud.net.IResponseAction;
+import com.chalmers.tda367.localfeud.net.IResponseListener;
 import com.chalmers.tda367.localfeud.net.IServerComm;
 import com.chalmers.tda367.localfeud.net.ServerComm;
 
@@ -13,6 +21,9 @@ import com.chalmers.tda367.localfeud.net.ServerComm;
 public class ChatActiveActivity extends AppCompatActivity
 {
     private IServerComm server;
+    private ImageButton postMessageButton;
+    private ListView chatMessageList;
+    private EditText chatMessageInput;
 
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -24,6 +35,18 @@ public class ChatActiveActivity extends AppCompatActivity
 
     private void initViews()
     {
+        postMessageButton = (ImageButton) findViewById(R.id.post_button);
+        chatMessageList = (ListView) findViewById(R.id.chat_message_list);
+        chatMessageInput = (EditText) findViewById(R.id.posttext);
 
+        postMessageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(chatMessageList,
+                        chatMessageInput.getText(),
+                        Snackbar.LENGTH_LONG)
+                        .show();
+            }
+        });
     }
 }
