4763933ed9d537d5e3e294c7808edb3bf57a6b08
Alfred Björk <alfbjo@student.chalmers.se>
2016-05-12 15:12:00 +0200

    IChatDataHandler

---
+++ project/app/src/main/java/com/chalmers/tda367/localfeud/data/handler/IChatDataHandler.java
@@ -0,0 +1,15 @@
+package com.chalmers.tda367.localfeud.data.handler;
+
+import com.chalmers.tda367.localfeud.data.Chat;
+import com.chalmers.tda367.localfeud.data.Post;
+
+import java.util.List;
+
+/**
+ * Created by Alfred on 2016-05-12.
+ */
+public interface IChatDataHandler {
+    void sendRequest(Post post, int userID, DataResponseListener<Chat> listener);
+
+    void getList(DataResponseListener<List<Chat>> listener);
+}
