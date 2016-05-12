500e68ec47280bea88471334dcdc858f779cd1a4
Alfred Björk <alfbjo@student.chalmers.se>
2016-05-12 15:17:26 +0200

    IMeDataHandler

---
+++ project/app/src/main/java/com/chalmers/tda367/localfeud/data/handler/IMeDataHandler.java
@@ -0,0 +1,10 @@
+package com.chalmers.tda367.localfeud.data.handler;
+
+import com.chalmers.tda367.localfeud.data.Me;
+
+/**
+ * Created by Alfred on 2016-05-12.
+ */
+public interface IMeDataHandler {
+    void get(DataResponseListener<Me> listener);
+}
