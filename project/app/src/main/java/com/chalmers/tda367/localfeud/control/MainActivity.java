37651d92289fd69558d271a8009b5292e5e23f30
David Söderberg <dasovm@gmail.com>
2016-05-26 12:12:58 +0200

    Location hotfix. Ej testad!

--- project/app/src/main/java/com/chalmers/tda367/localfeud/control/MainActivity.java
+++ project/app/src/main/java/com/chalmers/tda367/localfeud/control/MainActivity.java
@@ -1,8 +1,9 @@
 package com.chalmers.tda367.localfeud.control;
 
 import android.app.ActivityOptions;
-
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.support.v4.app.Fragment;
@@ -29,6 +30,8 @@ import com.chalmers.tda367.localfeud.data.Post;
 import com.chalmers.tda367.localfeud.data.handler.DataHandlerFacade;
 import com.chalmers.tda367.localfeud.data.handler.core.AbstractDataResponseListener;
 import com.chalmers.tda367.localfeud.data.handler.core.DataResponseError;
+import com.chalmers.tda367.localfeud.services.Location;
+import com.chalmers.tda367.localfeud.services.LocationPermissionError;
 import com.chalmers.tda367.localfeud.services.NotificationFacade;
 import com.facebook.FacebookSdk;
 import com.roughike.bottombar.BottomBar;
@@ -51,6 +54,11 @@ public class MainActivity extends AppCompatActivity implements PostAdapter.Adapt
         // Register this application for notifications
         if(NotificationFacade.checkPlayServices(this)){
             NotificationFacade.getInstance().registerForNotifications(this);
+        }
+        if (!Location.getInstance().isTracking()) {
+            try {
+                Location.getInstance().startTracking(getApplicationContext());
+            } catch (LocationPermissionError locationPermissionError) {}
         }
 
         setContentView(R.layout.activity_main);
@@ -237,6 +245,12 @@ public class MainActivity extends AppCompatActivity implements PostAdapter.Adapt
 
     @Override
     protected void onDestroy() {
+        try {
+                SharedPreferences prefs = getSharedPreferences("com.chalmers.tda367.localfeud", Context.MODE_PRIVATE);
+            prefs.edit().putLong("last_latitude", Double.doubleToRawLongBits(Location.getInstance().getLocation().getLatitude()));
+            prefs.edit().putLong("last_longitude", Double.doubleToRawLongBits(Location.getInstance().getLocation().getLongitude()));
+            Location.getInstance().stopTracking();
+        } catch (LocationPermissionError locationPermissionError) {}
         super.onDestroy();
     }
 }
--- project/app/src/main/java/com/chalmers/tda367/localfeud/services/Location.java
+++ project/app/src/main/java/com/chalmers/tda367/localfeud/services/Location.java
@@ -2,14 +2,12 @@ package com.chalmers.tda367.localfeud.services;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
-import android.util.Log;
-
-import com.chalmers.tda367.localfeud.util.TagHandler;
 
 /**
  * Created by ericwenn on 5/13/16.
@@ -19,6 +17,8 @@ public class Location implements ILocation {
     private android.location.Location lastKnownLocation;
     private static Location instance = null;
     private Context context;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
 
     public synchronized static Location getInstance() {
         if (instance == null) {
@@ -37,7 +37,7 @@ public class Location implements ILocation {
         }
 
         // Acquire a reference to the system Location Manager
-        LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
 
         lastKnownLocation = locationManager.getLastKnownLocation( LocationManager.GPS_PROVIDER );
         if( lastKnownLocation == null ) {
@@ -45,7 +45,7 @@ public class Location implements ILocation {
         }
 
         // Define a listener that responds to location updates
-        LocationListener locationListener = new LocationListener() {
+        locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(android.location.Location location) {
                 // Called when a new location is found by the network location provider.
@@ -69,20 +69,29 @@ public class Location implements ILocation {
 
     }
 
-
-    public android.location.Location getLocation() {
-        if (lastKnownLocation != null) {
-            return lastKnownLocation;
-        }
-        else {
-            try {
-                LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
-                return locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-            } catch (SecurityException e) {
-                Log.e(TagHandler.MAIN_TAG, "GPS Permission is not granted.");
+    public void stopTracking() throws LocationPermissionError {
+        if (context != null && locationManager != null && locationListener != null) {
+            if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                throw new LocationPermissionError();
             }
+            locationManager.removeUpdates(locationListener);
+            locationListener = null;
         }
-        return new android.location.Location(LocationManager.NETWORK_PROVIDER);
     }
 
+    public boolean isTracking() {
+        return locationManager != null && locationListener != null;
+    }
+
+    public android.location.Location getLocation() {
+        if (lastKnownLocation == null) {
+            SharedPreferences prefs = context.getSharedPreferences("com.chalmers.tda367.localfeud", Context.MODE_PRIVATE);
+            lastKnownLocation = new android.location.Location("");
+            double latitude = (double) prefs.getLong("last_latitude", 1);
+            double longitude = (double) prefs.getLong("last_longitude", 1);
+            lastKnownLocation.setLatitude(latitude);
+            lastKnownLocation.setLongitude(longitude);
+        }
+        return lastKnownLocation;
+    }
 }
